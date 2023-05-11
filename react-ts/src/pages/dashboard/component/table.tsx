573d85bdf7bdf46eed6a58bfe739880674f9cd6a
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-05-10 23:03:11 -0300

    feat/add table

--- react-ts/src/pages/dashboard/component/table.tsx
+++ react-ts/src/pages/dashboard/component/table.tsx
@@ -1,11 +1,92 @@
 export const Table = () => {
     return (
-        <div>
-            <ul>
-                <li>NOME</li>
-                <li>CARGO</li>
-                <li>E-MAIL</li>
-            </ul>
-        </div>
+        <table className="table-fixed border-[1px] border-[#F4F4F5] h-[594px]">
+            <thead className="border-b-[1px] border-[#F4F4F5]">
+                <tr>
+                    <th className="p-5">NOME</th>
+                    <th></th>
+                    <th></th>
+                    <th className="text-left">CARGO</th>
+                    <th className="text-left">E-MAIL</th>
+                    <th></th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td>botao</td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria luisa</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+                <tr className="h-16">
+                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
+                    <td>Maria</td>
+                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
+                    <td>Assistente</td>
+                    <td>maria@email.com</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
     )
 }
