989f975cf475e09151ed2dc324b890e00f80a2f9
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-05-10 23:03:57 -0300

    feat/ add inter font

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,14 +2,10 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import { router } from "./Router";
-import { NavBar } from "./components/navBar";
-import { Header } from "./components/header";
 
 function App() {
     return (
         <>
-            <Header />
-            <NavBar />
             <RouterProvider router={router} />
         </>
     )
