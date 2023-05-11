858b63f237ddf82c3a41f6b93ca9b34f2db6d8af
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-05-10 23:02:30 -0300

    config/add config global

--- react-ts/tailwind.config.js
+++ react-ts/tailwind.config.js
@@ -9,39 +9,28 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: 'Roboto, sans-serif',
+                sans: ['Inter, sans-serif'],
             },
             transitionDuration: {
                 900: '900ms',
             },
             colors: {
-                green: {
-                    300: '#00B37E',
-                    500: '#00875F',
-                    700: '#015F43',
-                    brasao: '#3e8e41',
-                    brasao2: '#0f8239',
-                },
                 blue: {
                     500: '#81D8F7',
-                },
-                orange: {
-                    500: '#FBA94C',
-                },
-                red: {
-                    500: '#F75A68',
+                    600: '#00A3ED',
+                    700: '#006B9E',
                 },
                 gray: {
-                    100: '#f0f0f3',
+                    100: '#F4F4F5',
                     150: '#Ececec',
                     200: '#C4C4CC',
                     300: '#8D8D99',
                     500: '#323238',
                     600: '#29292E',
-                    700: '#121214',
+                    700: '#5E666F',
                     900: '#09090A',
                 },
-                grayHover: '#CCCCCC',
+                blueHover: '#F8FAFC',
             },
         },
     },
