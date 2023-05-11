4601cc2a672dda35e7371569ae1c924fad64d6f1
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-05-10 23:02:58 -0300

    feat/add dashboard page

--- react-ts/src/pages/dashboard/dashboard.tsx
+++ react-ts/src/pages/dashboard/dashboard.tsx
@@ -1,17 +1,27 @@
+import { Header } from "../../components/header";
+import { NavBar } from "../../components/navBar";
 import { Table } from "./component/table";
 
 export default function Dashboard() {
     return (
-        <section>
-            <div>
-                <div>
-                    <h1>Colabordadores</h1>
-                    <p>Veja abaixo todos os colaboradores da sua empresa cadastrados na plataforma.</p>
+        <section className="flex flex-col h-screen">
+            <div className="flex flex-1">
+                <NavBar />
+                <div className="flex-1">
+                    <Header />
+                    <div className="grid ml-6 mr-6 mt-14 gap-10">
+                        <div className="flex justify-between items-center">
+                            <div className="grid gap-2">
+                                <h1>Colabordadores</h1>
+                                <p>Veja abaixo todos os colaboradores da sua empresa cadastrados na plataforma.</p>
+                            </div>
+                            <button type="button" value="colaborador" className="bg-blue-600 h-14 w-44 rounded text-white font-semibold text-base" >
+                                Novo colaborador
+                            </button>
+                        </div>
+                        <Table />
+                    </div>
                 </div>
-                <div>
-                    <input type="button" value="colaborador" placeholder="Novo Colaborador" />
-                </div>
-                <Table />
             </div>
         </section>
     )
