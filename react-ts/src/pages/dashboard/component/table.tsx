61ef29165bdee4e08abb978375b0bbc813714ab9
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-05-12 01:41:37 -0300

    feat/ table finished

--- react-ts/src/pages/dashboard/component/table.tsx
+++ react-ts/src/pages/dashboard/component/table.tsx
@@ -1,92 +1,67 @@
-export const Table = () => {
+import { TDataTableItem } from "../../../types"
+import { Dropdown } from "./dropdown";
+import { useState } from "react";
+
+type Props = {
+    data: TDataTableItem[];
+}
+
+export function Table({ data }: Props) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(5);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(data.length / itemsPerPage);
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+    }
+
     return (
-        <table className="table-fixed border-[1px] border-[#F4F4F5] h-[594px]">
-            <thead className="border-b-[1px] border-[#F4F4F5]">
-                <tr>
-                    <th className="p-5">NOME</th>
-                    <th></th>
-                    <th></th>
-                    <th className="text-left">CARGO</th>
-                    <th className="text-left">E-MAIL</th>
-                    <th></th>
-
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td>botao</td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria luisa</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-                <tr className="h-16">
-                    <td className="w-7 pl-5"><img className="rounded-full" src="https://picsum.photos/40/40" alt="" /></td>
-                    <td>Maria</td>
-                    <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3">Administrador</p></td>
-                    <td>Assistente</td>
-                    <td>maria@email.com</td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-
+        <>
+            <table className="table-fixed border-[1px] border-[#F4F4F5] h-[594px]">
+                <thead className="border-b-[1px] border-[#F4F4F5]">
+                    <tr>
+                        <th className="p-5">NOME</th>
+                        <th></th>
+                        <th></th>
+                        <th className="text-left">CARGO</th>
+                        <th className="text-left">E-MAIL</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {currentItems.map((item, index) => (
+                        <tr className="h-16" key={index}>
+                            <td className="w-7 pl-5"><img className="rounded-full" src={item.avatar} alt="" /></td>
+                            <td>{item.name}</td>
+                            <td className="text-center"><p className="bg-[#F7F7F8] w-fit rounded-2xl pt-1 pb-1 pl-3 pr-3 font-medium text-[#4A525B]">{item.permission}</p></td>
+                            <td>{item.role}</td>
+                            <td>{item.email}</td>
+                            <td>
+                                <Dropdown />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className="flex justify-center items-center">
+                {pageNumbers.map((number) => (
+                    <button
+                        key={number}
+                        className={`w-10 h-9 ${number === currentPage
+                            ? "border-[#C6CBD0] border-[1px] text-[#4A525B] bg-[#F8F8F8]"
+                            : "border-[#DCDFE3] border-[1px] text-[#727A83]"
+                            }`}
+                        onClick={() => setCurrentPage(number)}
+                    >
+                        {number}
+                    </button>
+                ))}
+            </div>
+        </>
     )
 }
