a7973b6d551bc848140f3a4176c508693bec1fe7
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-05-12 01:42:59 -0300

    feat/ finished navbar

--- react-ts/src/components/navBar.tsx
+++ react-ts/src/components/navBar.tsx
@@ -1,30 +1,52 @@
-export const NavBar = () => {
+import Logo from "../assets/Logo.svg"
+import { useState, useEffect } from 'react';
+
+interface NavItem {
+    label: string;
+    isFocus: boolean;
+}
+
+export function NavBar() {
+    const [focusedIndex, setFocusedIndex] = useState<number | null>(null); // armazena o índice do item em foco
+
+    const handleFocus = (index: number) => {
+        setFocusedIndex(index);
+        localStorage.setItem('focusedIndex', index.toString()); // armazena o índice do item em foco no localStorage
+    }
+
+    useEffect(() => {
+        const storedIndex = localStorage.getItem('focusedIndex'); // recupera o índice do item em foco do localStorage
+        if (storedIndex) {
+            setFocusedIndex(parseInt(storedIndex));
+        }
+    }, []);
+
+    const navItems: NavItem[] = [
+        { label: "Dashboard", isFocus: false },
+        { label: "Conversas", isFocus: false },
+        { label: "Minhas solicitações", isFocus: false },
+        { label: "Colaboradores", isFocus: false },
+        { label: "Advogado Virtual", isFocus: false },
+        { label: "Contratos", isFocus: false },
+    ];
+
+    if (focusedIndex !== null) {
+        navItems[focusedIndex].isFocus = true;
+    }
+
     return (
-        <aside className="w-72 h-auto border-gray-100 border-r-[1px]">
+        <aside className="grid w-72 h-auto border-gray-100 border-r-[1px]">
             <div className="mt-12 pl-10 pr-24">
-                <h1>adv.CO</h1>
+                <img className="w-40" src={Logo} alt="Logo Legal Cloud" />
             </div>
             <nav className="grid mt-12 gap-2">
-                <div className="flex items-center h-12  hover:bg-blueHover hover:border-[#006B9E] hover:border-r-2">
-                    <a className="pt-3 pb-3 ml-10 w-full focus:text-blue-700" href="/">Dashboard</a>
-                </div>
-                <div className="flex items-center h-12 hover:bg-blueHover hover:border-[#006B9E] hover:border-r-2">
-                    <a className="pt-3 pb-3 ml-10 w-full focus:text-blue-700" href="/">Conversas</a>
-                </div>
-                <div className="flex items-center h-12 hover:bg-blueHover hover:border-[#006B9E] hover:border-r-2">
-                    <a className="pt-3 pb-3 ml-10 w-full focus:text-blue-700" href="/">Minhas solicitações</a>
-                </div>
-                <div className="flex items-center h-12 hover:bg-blueHover hover:border-[#006B9E] hover:border-r-2">
-                    <a className="pt-3 pb-3 ml-10 w-full focus:text-blue-700" href="/">Colaboradores</a>
-                </div>
-                <div className="flex items-center h-12 hover:bg-blueHover hover:border-[#006B9E] hover:border-r-2">
-                    <a className="pt-3 pb-3 ml-10 w-full focus:text-blue-700" href="/">Advogado Virtual</a>
-                </div>
-                <div className="flex items-center h-12 hover:bg-blueHover hover:border-[#006B9E] hover:border-r-2">
-                    <a className="pt-3 pb-3 ml-10 w-full focus:text-blue-700" href="/">Contratos</a>
-                </div>
+                {navItems.map((item, index) => (
+                    <div key={index} className={`flex items-center h-12 hover:bg-blueHover hover:border-[#006B9E] hover:border-r-2 ${item.isFocus ? 'bg-blueHover border-r-2 border-[#006B9E]' : ''} `} onFocus={() => handleFocus(index)} >
+                        <a className={`pt-3 pb-3 ml-10 w-full focus:text-blue-700 focus:font-semibold ${item.isFocus ? 'text-blue-700 font-semibold' : ''} `} href="/">{item.label}</a>
+                    </div>
+                ))}
             </nav>
-            <div className="mt-80 border-gray-100 m-10 border-t-[1px]">
+            <div className="self-end border-gray-100 m-10 border-t-[1px]">
                 <div className="mt-8 gap-8 grid">
                     <div>
                         <a href="">Configurações</a>
